76f2bf00b447dfb58c6ab2a69a15918983ac3ba7
Ryosuke Narita <>
2022-04-24 19:47:19 +0900

    上に戻るボタン追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <AppHeader></AppHeader>
+  <AppHeader />
   <slot></slot>
+  <ScrollTop />
 </template>
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -3,6 +3,7 @@ import PrimeVue from 'primevue/config'
 import Button from 'primevue/button'
 import Sidebar from 'primevue/sidebar'
 import Avatar from 'primevue/avatar'
+import ScrollTop from 'primevue/scrolltop'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue)
@@ -12,4 +13,5 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('Sidebar', Sidebar)
   // MISC
   nuxtApp.vueApp.component('Avatar', Avatar)
+  nuxtApp.vueApp.component('ScrollTop', ScrollTop)
 })
