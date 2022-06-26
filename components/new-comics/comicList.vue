8f2777283062dff8d17d01735a150ef38d80e270
Ryosuke Narita <>
2022-06-26 15:33:23 +0900

    前月のコミック一覧を表示

---
+++ components/new-comics/comicList.vue
@@ -0,0 +1,220 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+
+import { storeToRefs } from 'pinia'
+import {
+  Label,
+  Comic,
+  useNewComicStore,
+  idKey,
+  titleKey,
+  authorKey,
+  labelKey,
+  dateKey,
+} from '~/store/newComic'
+
+type Props = {
+  prev?: boolean
+}
+const props = withDefaults(defineProps<Props>(), {
+  prev: false,
+})
+
+const newComicStore = useNewComicStore()
+const { comicsListGroupByDate, labels, prevComicsListGroupByDate, prevLabels } =
+  storeToRefs(newComicStore)
+const comicsList = computed(() =>
+  props.prev ? prevComicsListGroupByDate : comicsListGroupByDate
+)
+const labelList = computed(() => (props.prev ? prevLabels : labels))
+const label = ref<Label[]>()
+// 選択したレーベルのコミックか
+const isSelectedLabelComic = (comic: Comic) => {
+  if (!label.value || label.value.length === 0) return true
+  return label.value.findIndex((p) => p.key === comic[labelKey]) > -1
+}
+
+const releaseDate = ref()
+const isSelectedReleaseDate = (date: string): boolean => {
+  if (!releaseDate.value || releaseDate.value.length === 0) return true
+  return (
+    releaseDate.value.findIndex((d: Date) => {
+      return dayjs(d).format('M月D日') === date
+    }) > -1
+  )
+}
+const isSelectedReleaseDateComic = (comic: Comic): boolean => {
+  if (!releaseDate.value || releaseDate.value.length === 0) return true
+  return (
+    releaseDate.value.findIndex((d: Date) => {
+      return dayjs(d).format('M月D日') === comic[dateKey]
+    }) > -1
+  )
+}
+
+const dailyComicCount = (date: any) => {
+  const comicList = comicsList.value.value.find(
+    (obj) => obj.date === `${date.month + 1}月${date.day}日`
+  )
+  if (!comicList) return ''
+  const filterdComicList = comicList.list.filter((c) => isSelectedLabelComic(c))
+  if (!filterdComicList.length) return ''
+  return `(${filterdComicList.length})`
+}
+
+const calendar = ref()
+onMounted(async () => {
+  props.prev
+    ? await newComicStore.getPrevComics()
+    : await newComicStore.getNewComics()
+
+  if (!comicsList.value.value || comicsList.value.value.length === 0) {
+    return
+  }
+  // 取得月
+  const month = comicsList.value.value[0].date.charAt(0)
+  // 月の後半は翌月の分を取得するのでカレンダーも翌月にずらす
+  if (month !== calendar.value.currentMonth + 1) {
+    calendar.value.currentMonth++
+  }
+})
+
+const copyComics = () => {
+  // 一覧作成
+  const comics = comicsList.value.value
+    .filter(
+      (obj) =>
+        isSelectedReleaseDate(obj.date) &&
+        obj.list.some((comic) => isSelectedLabelComic(comic))
+    )
+    .map(
+      (obj) =>
+        `【${obj.date}】\n` +
+        `${obj.list
+          .filter((comic) => isSelectedLabelComic(comic))
+          .map(
+            (comic) =>
+              `「${comic[titleKey]}」\n${comic[authorKey]}\n${comic[labelKey]}\n`
+          )
+          .join('\n')}`
+    )
+    .join('\n')
+
+  // クリップボードにコピー
+  navigator.clipboard.writeText(comics)
+}
+</script>
+
+<template>
+  <Card>
+    <template #content>
+      <DataView :value="comicsList.value" data-key="date">
+        <template #header>
+          <div class="condition-row">
+            <Calendar
+              id="icon"
+              ref="calendar"
+              v-model="releaseDate"
+              show-icon
+              date-format="yy-mm-dd"
+              selection-mode="multiple"
+              placeholder="発売日を選択"
+            >
+              <template #date="slotProps">
+                <div
+                  class="flex flex-column justify-content-center align-items-center"
+                >
+                  <div>
+                    {{ slotProps.date.day }}
+                  </div>
+                  <div class="text-xs">
+                    {{ dailyComicCount(slotProps.date) }}
+                  </div>
+                </div>
+              </template>
+            </Calendar>
+            <MultiSelect
+              v-model="label"
+              :options="labelList.value"
+              option-label="name"
+              placeholder="レーベルを選択"
+              display="chip"
+            >
+              <template #option="slotProps">{{
+                `${slotProps.option.name}(${slotProps.option.count})`
+              }}</template>
+            </MultiSelect>
+            <Button
+              v-tooltip="'一覧をクリップボードにコピー'"
+              icon="pi pi-copy"
+              class="p-button-secondary"
+              @click="copyComics"
+            />
+          </div>
+        </template>
+        <template #list="slotProps">
+          <div
+            v-show="isSelectedReleaseDate(slotProps.data.date)"
+            class="col-12 p-3"
+          >
+            <div :id="slotProps.data.date" class="mb-3 text-lg">
+              {{ `${slotProps.data.date}` }}
+            </div>
+            <div
+              v-for="comic in slotProps.data.list"
+              v-show="
+                isSelectedLabelComic(comic) && isSelectedReleaseDateComic(comic)
+              "
+              :key="comic[idKey]"
+              class="mb-3 pl-7 pb-3 border-bottom-1 border-300"
+            >
+              <NuxtLink
+                :href="`https://www.amazon.co.jp/s?k=${comic[titleKey]}`"
+                target="_blank"
+                class="comic-title"
+              >
+                {{ comic[titleKey] }}
+              </NuxtLink>
+              <div class="flex justify-content-between mt-2">
+                <div>{{ comic[authorKey] }}</div>
+                <div>{{ comic[labelKey] }}</div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </DataView>
+    </template>
+  </Card>
+</template>
+
+<style scoped lang="scss">
+@import 'primeflex/primeflex.scss';
+
+.condition-row {
+  @include styleclass(
+    'md:flex lg:justify-content-between lg:align-items-center mb-2'
+  );
+
+  column-gap: 1rem;
+
+  ::v-deep .p-calendar {
+    @include styleclass('md:w-6 w-full');
+  }
+
+  .p-multiselect {
+    @include styleclass('md:w-6 w-full');
+  }
+}
+
+::v-deep .p-dataview-content {
+  @include styleclass('overflow-y-scroll h-screen');
+}
+
+.comic-title {
+  @include styleclass('font-bold text-lg');
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+</style>
--- store/newComic.ts
+++ store/newComic.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { defineStore } from 'pinia'
 // 「ID」列のキー
 export const idKey = 0
@@ -29,52 +30,15 @@ export type Label = {
 export const useNewComicStore = defineStore('newComic', {
   state: () => ({
     comics: [] as Comic[],
+    prevComics: [] as Comic[],
   }),
   getters: {
-    comicsListGroupByDate: (state): ComicListGroupByDate[] => {
-      // 日付でグルーピングしたコミック一覧
-      return state.comics.reduce(
-        (result: ComicListGroupByDate[], current: Comic) => {
-          // 該当日のオブジェクトを取得
-          const comicListObject = result.find(
-            (obj) => obj.date === current[dateKey]
-          )
-          if (comicListObject) {
-            // ある場合はコミックを追加
-            comicListObject.list.push(current)
-          } else {
-            // ない場合は該当日のオブジェクトを追加
-            result.push({ date: current[dateKey], list: [current] })
-          }
-          return result
-        },
-        []
-      )
-    },
-    labels: (state): Label[] => {
-      // レーベル一覧
-      return (
-        state.comics
-          .reduce((result: Label[], current: Comic) => {
-            // レーベルを取得
-            const label = result.find((obj) => obj.key === current[labelKey])
-            if (label) {
-              // ある場合はカウント増やす
-              label.count++
-            } else {
-              // ない場合はレーベルを追加
-              result.push({
-                key: current[labelKey],
-                name: current[labelKey],
-                count: 1,
-              })
-            }
-            return result
-          }, [])
-          // 多い順でソート
-          .sort((a, b) => b.count - a.count)
-      )
-    },
+    comicsListGroupByDate: (state): ComicListGroupByDate[] =>
+      groupComicsByDate(state.comics),
+    prevComicsListGroupByDate: (state): ComicListGroupByDate[] =>
+      groupComicsByDate(state.prevComics),
+    labels: (state): Label[] => getLabels(state.comics),
+    prevLabels: (state): Label[] => getLabels(state.prevComics),
   },
   actions: {
     async getNewComics() {
@@ -83,5 +47,53 @@ export const useNewComicStore = defineStore('newComic', {
       )
       this.comics = list
     },
+    async getPrevComics() {
+      // 月の前半は当月、後半は前月を指定
+      const yearMonth =
+        dayjs().date() < 16
+          ? dayjs().format('YYYY/MM')
+          : dayjs().subtract(1, 'day').format('YYYY/MM')
+      const { list }: { list: Comic[] } = await $fetch(
+        `/rakuten/event/book/comic/calendar/${yearMonth}/js/booklist.json`
+      )
+      this.prevComics = list
+    },
   },
 })
+
+// 日付でグルーピングしたコミック一覧
+const groupComicsByDate = (comics: Comic[]) =>
+  comics.reduce((result: ComicListGroupByDate[], current: Comic) => {
+    // 該当日のオブジェクトを取得
+    const comicListObject = result.find((obj) => obj.date === current[dateKey])
+    if (comicListObject) {
+      // ある場合はコミックを追加
+      comicListObject.list.push(current)
+    } else {
+      // ない場合は該当日のオブジェクトを追加
+      result.push({ date: current[dateKey], list: [current] })
+    }
+    return result
+  }, [])
+
+// コミック一覧からレーベル一覧を取得
+const getLabels = (comics: Comic[]) =>
+  comics
+    .reduce((result: Label[], current: Comic) => {
+      // レーベルを取得
+      const label = result.find((obj) => obj.key === current[labelKey])
+      if (label) {
+        // ある場合はカウント増やす
+        label.count++
+      } else {
+        // ない場合はレーベルを追加
+        result.push({
+          key: current[labelKey],
+          name: current[labelKey],
+          count: 1,
+        })
+      }
+      return result
+    }, [])
+    // 多い順でソート
+    .sort((a, b) => b.count - a.count)
