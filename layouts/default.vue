c6dffde3d1d3e515350a9b4c9e51f40cc8fa107f
Ryosuke Narita <>
2022-07-28 23:14:04 +0900

    meta削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,38 +3,6 @@ useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - Web Portfolio` : 'Web Portfolio'
   },
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    {
-      hid: 'description',
-      name: 'description',
-      content: "Welcome to Ryo's Portfolio!",
-    },
-    {
-      hid: 'og:site_name',
-      property: 'og:site_name',
-      content: "Ryo's Portfolio",
-    },
-    { hid: 'og:type', property: 'og:type', content: 'website' },
-    {
-      hid: 'og:url',
-      property: 'og:url',
-      content: 'https://ryo-portfolio.netlify.app',
-    },
-    { hid: 'og:title', property: 'og:title', content: "Ryo's Portfolio" },
-    {
-      hid: 'og:description',
-      property: 'og:description',
-      content: "Welcome to Ryo's Portfolio!",
-    },
-    {
-      hid: 'og:image',
-      property: 'og:image',
-      content: 'https://ryo-portfolio.netlify.app/_nuxt/icon.c1de94d3.jpg',
-    },
-    { name: 'twitter:card', content: 'summary' },
-    { name: 'twitter:site', content: '@ryo_chan_13' },
-  ],
 })
 </script>
 <template>
